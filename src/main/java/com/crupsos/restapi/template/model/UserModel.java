16dc19fef8811a6b703e43775b1942c6b1f7ba01
Nils <nilsvennemann06@gmail.com>
2020-11-03 10:47:21 +0100

    user model added

---
+++ src/main/java/com/crupsos/restapi/template/model/UserModel.java
@@ -0,0 +1,22 @@
+package com.crupsos.restapi.template.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "users")
+public class UserModel {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
+    private String email;
+    private String password;
+
+}
